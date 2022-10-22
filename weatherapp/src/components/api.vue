4da3c2459a1d5b049b39b3e47f80ac6d17d2a3a6
LucasRi <lucasrimocii@gmail.com>
2022-10-22 19:21:17 +0200

    New Setup

---
+++ weatherapp/src/components/api.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>hello</h1>
+    {{ info }}
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'hello',
+    data() {
+        return {
+            info: null
+        }
+    },
+    mounted() {
+        axios.get('https://api.openweathermap.org/data/2.5/weather?lat=48.148598&lon=17.107748&units=metric&lang=sk&appid=9a940b74ba054ec407a90bf5159392db')
+        .then(response => (this.info = response))
+
+        }
+}
+</script>
